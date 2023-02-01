b9120e46a09bf07ecd94bac7a0bf6bc18974c7c2
lorena <lorena.anaya.diaz@gmail.com>
2023-02-01 17:50:57 +0100

    add eslint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['xo', 'prettier'],
+  overrides: [
+    {
+      extends: ['xo-typescript', 'prettier'],
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': [
+          'error',
+          'interface',
+        ],
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'object-curly-spacing': ['Error', 'always'],
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        argsIgnorePattern: /^_/.source,
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: /^_$/.source,
+        indent: ['error', 2],
+        'object-curly-spacing': ['Error', 'always'],
+      },
+    ],
+  },
+};
